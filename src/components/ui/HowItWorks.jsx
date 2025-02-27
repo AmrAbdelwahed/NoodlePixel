159dce1f66be3c18dc15d185c39bb90e63f652e8
AmrAbdelwahed <amrabdelwahed00@gmail.com>
2025-02-26 21:05:22 -0500

    modified button color on hover

--- src/components/ui/HowItWorks.jsx
+++ src/components/ui/HowItWorks.jsx
@@ -81,7 +81,7 @@ const HowItWorks = () => {
           <Link
             to="/companies-for-hire"
             target="_blank"
-            className="inline-block px-8 py-4 text-xl font-bold text-white bg-[#2A69EF] rounded-lg text-center hover:bg-blue-600 transition-colors w-full sm:w-80 sm:mx-2"
+            className="inline-block px-8 py-4 text-xl font-bold text-white bg-[#2A69EF] rounded-lg text-center hover:bg-gray-900 transition-colors duration-300 w-full sm:w-80 sm:mx-2"
           >
             Companies Start Here
           </Link>
@@ -90,7 +90,7 @@ const HowItWorks = () => {
           <Link
             to="/security-guard-register"
             target="_blank"
-            className="inline-block px-8 py-4 text-xl font-bold text-white bg-[#2A69EF] rounded-lg text-center hover:bg-blue-600 transition-colors w-full sm:w-80 sm:mx-2"
+            className="inline-block px-8 py-4 text-xl font-bold text-white bg-[#2A69EF] rounded-lg text-center hover:bg-gray-900 transition-colors duration-300 w-full sm:w-80 sm:mx-2"
           >
             Security Guards Start Here
           </Link>
